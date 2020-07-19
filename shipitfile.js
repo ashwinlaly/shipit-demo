eb0a92ab084613f812bb6727dc4f849674536c3d
unknown <ashwinlaly@gmail.com>
2020-07-19 16:55:56 +0530

    path set for shipit

--- shipitfile.js
+++ shipitfile.js
@@ -6,7 +6,23 @@ module.exports = shipit => {
 
     shipit.initConfig({
         default : {
-            deployTo : '/var/www/'
+            deployTo : '/home/ashwin/shipit',
+            repositoryUrl : 'https://github.com/ashwinlaly/shipit-demo.git',
+            keepReleases: 5,
+            shared: {
+                overwrite: true,
+                dirs: ['node_modules']
+            }
+        },
+        production: {
+            servers: 'ashwin@172.16.22.2'
         }
-    })
+    });
+
+    const path = require('path');
+    const ecosystemFilePath = path.join(
+      shipit.config.deployTo,
+      'shared',
+      'ecosystem.config.js'
+    );
 }
